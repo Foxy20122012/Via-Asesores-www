3e4e2a3d3b5057b2dad7ba6699bea9a0e2879e2d
Miguel Enrique <enriquemerlos78@gmail.com>
2023-09-11 20:57:23 -0600

    Volviendo reutilizables los componentes

--- src/components/InfoBlock.jsx
+++ src/components/InfoBlock.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const InfoBlock = (props) => {
-  const { title, text, imageUrl, imageAlt } = props;
+  const { title, text, imageUrl, imageAlt,backgroundColor } = props;
+
+  const bgStyle = {
+    background: backgroundColor || "transparent",// Fondo predeterminado
+  };
 
   return (
-    <div className="bg-sky-700 p-8 text-white">
+    <div className="p-8 text-white" style={bgStyle}>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 items-center">
         <div className="col-span-1 sm:col-span-2 md:col-span-1 lg:col-span-1 flex flex-col">
           <h1 className="text-2xl font-bold uppercase mx-2 my-4 justify-start">
